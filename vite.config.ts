7458bb199acab331e9dc5d8830396fa7bf62f0d3
Lorenzo S <110693047+bitlimefinance@users.noreply.github.com>
2022-11-27 19:26:06 +0100

    Vite compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		viteCompression()
+	]
 };
 
 export default config;
