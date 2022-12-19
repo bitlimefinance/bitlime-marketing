75e29598aea4f8bb18d4f88b19b4f5003a87edca
Bitlime <110693047+bitlimefinance@users.noreply.github.com>
2022-12-19 03:28:56 +0100

    Major update

---
+++ src/lib/components/general/icon.svelte
@@ -0,0 +1,14 @@
+
+<script lang="ts">
+	import type { Heroicon } from "$lib/icons/icons";
+
+    export let solid: boolean = true;
+    export let icon: Heroicon = 'face-smile'; // icons from https://heroicons.com/
+
+    $: path = '../../icons/' + (solid ? 'solid' : 'outline') + '/' + icon + '.svelte';
+
+</script>
+
+{#await import(path) then iconComponent}
+    <svelte:component this={iconComponent.default}/>
+{/await}
